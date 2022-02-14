6cc0fc9eedd2c44ecfc8310ad5acad404cb7f7ff
alex-coch <a@deonop.com>
2022-02-14 11:26:11 +0400

    merge

--- Data Science/python/test.py
+++ Data Science/python/test.py
@@ -6,10 +6,27 @@ import pandas as pd
 
 df = pd.read_csv('data_sf.csv')
 #print(df.iloc[0])
-#print(df.info())
+# print(df.info())
 #print(df.describe(include = ['object']))
 # print(df.Value.min())
 
-val = df[df.Aggression == df.Aggression.max()].ShotPower.mean() / df[df.Aggression == df.Aggression.min()].ShotPower.mean()
+df = df.pivot_table(values=['SprintSpeed'],
+index=['Club'],
+columns=['Position'],
+aggfunc='mean',
+margins=True,
+fill_value=0)
 
-print(val)
+# val = df[df['Club'] == 'FC Barcelona'].groupby(['Nationality']).Balance.max()
+# val = df.loc[df['Nationality'] == 'Dominican Republic'][['Name','Club','Wage','Age','ShotPower']]
+# print(val)
+# df = df.loc[df['Club'].isin(['FC Barcelona','Real Madrid','Juventus','Manchester United'])].pivot_table(values=['Name'],
+# index=['Nationality'],
+# columns=['Club'],
+# aggfunc='count',
+# fill_value=0)
+# print(df[df['Club'] == 'Manchester City'])
+# df = df.reindex(df['Position'].sort_values(by='ST', ascending=False).index)
+
+print(df.sort_values(by=[('SprintSpeed',  'ST')], ascending=False))
+# .sort_values(by=[('SprintSpeed',  'CF')], ascending=False)
