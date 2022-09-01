157f46bbafdff31e6bea04dfa301618b2a05656c
alex-coch <a@deonop.com>
2022-09-01 16:12:09 +0400

    main.py

--- Data Science/linear algebra/main.py
+++ Data Science/linear algebra/main.py
@@ -5,3 +5,43 @@ b = np.matrix([1, 4, 5, 0]).reshape(4,1)
 w = np.linalg.inv(a.T.dot(a)).dot(a.T).dot(b)
 # e = b - a.dot(w)
 print(w)
+
+import numpy as np
+
+def rosen(x):
+# Функция Розенброка
+    return np.sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0, axis=0)
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+# Настраиваем 3D график
+fig = plt.figure(figsize=[15, 10])
+ax = fig.gca(projection='3d')
+
+# Задаем угол обзора
+ax.view_init(45, 30)
+
+# Создаем данные для графика
+X = np.arange(-2, 2, 0.1)
+Y = np.arange(-1, 3, 0.1)
+X, Y = np.meshgrid(X, Y)
+Z = rosen(np.array([X,Y]))
+
+# Рисуем поверхность
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm)
+plt.show()
+
+from scipy.optimize import minimize
+x0 = np.array([1.3, 0.7, 0.8, 1.9, 1.2])
+res = minimize(rosen, x0, method='powell',
+    options={'xtol': 1e-8, 'disp': True})
+print(res.x)
+
+from scipy.optimize import minimize
+x0 = np.array([1.3, 0.7, 0.8, 1.9, 1.2])
+res = minimize(rosen, x0, method='nelder-mead',
+    options={'xtol': 1e-8, 'disp': True})
+print(res.x)
