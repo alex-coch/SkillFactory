b8f4408a8b105fcd0a440c96b0356c2a4c643787
drinkwat <drinkwat@student.21-school.ru>
2022-12-01 14:24:32 +0400

    plot_learning_curve

---
+++ Data Science/Classification/plot_learning_curve.py
@@ -0,0 +1,29 @@
+def plot_learning_curve(model, X, y, cv, scoring="f1", ax=None, title=""):
+    # Вычисляем координаты для построения кривой обучения
+    train_sizes, train_scores, valid_scores = model_selection.learning_curve(
+        estimator=model,  # модель
+        X=X,  # матрица наблюдений X
+        y=y,  # вектор ответов y
+        cv=cv,  # кросс-валидатор
+        scoring=scoring,  # метрика
+    )
+    # Вычисляем среднее значение по фолдам для каждого набора данных
+    train_scores_mean = np.mean(train_scores, axis=1)
+    valid_scores_mean = np.mean(valid_scores, axis=1)
+    # Если координатной плоскости не было передано, создаём новую
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 4))  # фигура + координатная плоскость
+    # Строим кривую обучения по метрикам на тренировочных фолдах
+    ax.plot(train_sizes, train_scores_mean, label="Train")
+    # Строим кривую обучения по метрикам на валидационных фолдах
+    ax.plot(train_sizes, valid_scores_mean, label="Valid")
+    # Даём название графику и подписи осям
+    ax.set_title("Learning curve: {}".format(title))
+    ax.set_xlabel("Train data size")
+    ax.set_ylabel("Score")
+    # Устанавливаем отметки по оси абсцисс
+    ax.xaxis.set_ticks(train_sizes)
+    # Устанавливаем диапазон оси ординат
+    ax.set_ylim(0, 1)
+    # Отображаем легенду
+    ax.legend()
