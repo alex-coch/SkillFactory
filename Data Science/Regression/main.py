4d6addde0943e64a7d80a2140c21799dc96d2cc6
drinkwat <drinkwat@student.21-school.ru>
2022-12-12 10:47:07 +0400

    linear algebra

--- Data Science/Regression/main.py
+++ Data Science/Regression/main.py
@@ -1,10 +1,10 @@
 from sklearn import metrics
 import numpy as np
 
-x = np.array([1, 3, 2, 5])
-y = np.array([2, 3, -1, 4])
-print(metrics.r2_score(y, x))
+x = np.array([1.23, 2.35, 2.75])
+y = np.array([1.01, 12.3, 2.74])
+print(metrics.mean_squared_error(y, x))
 
-rss = np.sum((y - x) ** 2)
-tss = np.sum((y - y.mean()) ** 2)
-print(1 - rss / tss)
+# rss = np.sum((y - x) ** 2)
+# tss = np.sum((y - y.mean()) ** 2)
+# print(1 - rss / tss)
